113bf0e9aa4725a9052c1899fe09839aa0862856
F. Waskito <fajar.waskito10@gmail.com>
2024-08-20 01:02:13 +0700

    feat: add solution682.js

---
+++ src/problem682/solution682.js
@@ -0,0 +1,54 @@
+/**
+ * 682. Baseball Game
+ *
+ * - Difficulty: Easy
+ * - Topics: Array, Stack, Simulation
+ */
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+function calPoints(operations) {
+  let numbers = [];
+  for (let i = 0; i < operations.length; i++) {
+    if (Number(operations[i])) {
+      numbers.push(Number(operations[i]));
+    } else {
+      switch (operations[i]) {
+        case "+":
+          numbers.push(numbers.slice(-1)[0] + numbers.slice(-2)[0]);
+          break;
+        case "D":
+          numbers.push(numbers.slice(-1)[0] * 2);
+          break;
+        case "C":
+          numbers.pop();
+          break;
+      }
+    }
+  }
+
+  let sum = 0;
+  numbers.forEach((x) => {
+    sum += x;
+  });
+
+  return sum;
+}
+
+// operations = ["5", "2", "C", "D", "+"];
+// expected: 30
+
+// let operations = ["5", "-2", "4", "C", "D", "9", "+", "+"];
+// expected: 27
+
+let operations = ["1", "C"];
+// expected: 0
+
+const start = performance.now();
+
+console.log(calPoints(operations));
+
+const end = performance.now();
+console.log(`\nExecution time: ${end - start} ms.`);
